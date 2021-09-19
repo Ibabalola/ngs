fde26edfd53c66b6a4c0db9162f065ae431fc12b
Isaac Babalola <info@isaacbabalola.co.uk>
2021-09-19 13:57:38 +0100

    adding challenge 2

--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,20 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+
+const interval = 1000;
+let counter = 0;
+const limit = 5
+const print = () => {
+  console.log('Hello World');
+}
+
+
+const timerId = setInterval(() => {
+  print();
+  counter += 1;
+
+  if (counter >= limit) {
+    clearInterval(timerId);
+  }
+}, interval);
