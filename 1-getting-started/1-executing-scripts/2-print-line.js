a992e624a15bdb7939fb37e936c04e2483c8d26e
Isaac Babalola <info@isaacbabalola.co.uk>
2021-10-02 20:14:09 +0100

    adding information for course

--- 1-getting-started/1-executing-scripts/2-print-line.js
+++ 1-getting-started/1-executing-scripts/2-print-line.js
@@ -1,1 +1,5 @@
-console.log('Pluralsight rocks');
+// Every node module is wrapped in a function which passes the following objects
+// function (exports, module, require, __filename, __dirname) {
+console.log('Pluralsight rocks', arguments);
+// return module.exports;
+// } (module.exports)
